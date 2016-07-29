6953f6d2ab6804b5d6ef6e949fe7a0ebe49f0b71
tngan <tonynwk919@gmail.com>
2016-07-30 02:45:19 +0800

    Messy and messy and messy

---
+++ facerec_video.cpp
@@ -0,0 +1,58 @@
+#include "stdio.h"
+#include "math.h"
+#include "opencv/cv.h"
+#include <opencv/highgui.h>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+#define blue  CV_RGB(0,0,255)
+#define green CV_RGB(0,255,0)
+#define red   CV_RGB(255,0,0)
+#define white CV_RGB(255,255,255)
+#define black CV_RGB(0,0,0)
+
+int main()
+{
+	CascadeClassifier face_cascade;
+	if(!face_cascade.load("./metadata/lbpcascade_frontalface.xml")) {
+		printf("Error loading cascade file for face");
+		return 1;
+	}
+	VideoCapture capture(0); //-1, 0, 1 device id
+	if(!capture.isOpened())
+	{
+		printf("error to initialize camera");
+		return 1;
+	}
+	Mat cap_img,gray_img;
+	vector<Rect> faces, eyes;
+	while(1)
+	{
+		capture >> cap_img;
+		waitKey(10);
+		cvtColor(cap_img, gray_img, CV_BGR2GRAY);
+		cv::equalizeHist(gray_img,gray_img);
+		face_cascade.detectMultiScale(gray_img, faces, 1.1, 10, CV_HAAR_SCALE_IMAGE | CV_HAAR_DO_CANNY_PRUNING, cvSize(0,0), cvSize(300,300));
+		for(int i=0; i < faces.size();i++)
+		{
+			Point pt1(faces[i].x+faces[i].width, faces[i].y+faces[i].height);
+			Point pt2(faces[i].x,faces[i].y);
+			Mat faceROI = gray_img(faces[i]);
+			rectangle(gray_img, pt1, pt2, cvScalar(0,255,0), 2, 8, 0);
+      //line(gray_img, Point((pt2.x - pt1.x)/2, pt1.y), Point((pt2.x - pt1.x)/2, (pt2.y-pt1.y)/2), cvScalar(0,255,0));
+      line(gray_img, Point((pt1.x + pt2.x)/2,pt2.y), Point((pt1.x + pt2.x)/2,(pt1.y + pt2.y)/2), Scalar(255,130,106,255), 2);
+      line(gray_img, Point(pt2.x,(pt1.y+pt2.y)/2), Point(pt1.x,(pt1.y+pt2.y)/2), Scalar(255,130,106,255), 2);
+		}
+		imshow("Result", gray_img);
+		waitKey(3);
+		char c = waitKey(3);
+		if(c == 27)
+			break;
+	}
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 #include "opencv2/opencv.hpp"
 #include "util.cpp"
 #include "corners.cpp"
+#include "opencv2/video/video.hpp"
 
 using namespace cv;
 using namespace std;
 
-#define KERNEL_SIZE 
+#define KERNEL_SIZE 3
 #define SIGMA_X 4 
 /*
  * All the references
@@ -33,6 +34,13 @@ int main()
   Mat blur_image;
 
   //Size ksz = Size(KERNEL_SIZE, KERNEL_SIZE);
+  CascadeClassifier face_detector;
+  //string cascade_name = "haarcascade_frontalface_alt.xml";
+  face_detector.load("./metadata/haarcascade_frontalface_alt.xml");
+  // Vector of returned faces
+	vector<Rect> found;
+
+  Mat edges;
 
   while(1)
   {
@@ -42,17 +50,28 @@ int main()
     cvtColor(raw_image, gray_image, CV_BGR2GRAY);
     //imshow("raw", raw_image);
 
+		cvtColor(raw_image, edges, CV_BGR2GRAY); //transform to a grayscale image
+
+		// Detect faces
+		face_detector.detectMultiScale(edges, found, 1.1, 2, 0 | 2, Size(80,80), Size(200,200));
+
+		// Draw the results into mat retrieved from webcam
+		if (found.size() > 0) {
+			for (int i = 0; i <= found.size() - 1; i++) {
+				rectangle(edges, found[i].br(), found[i].tl(), Scalar(0, 0, 0), 1, 8, 0);
+				rectangle(raw_image, found[i].br(), found[i].tl(), Scalar(0, 0, 0), 1, 8, 0);
+			}
+		}
+
+		imshow("original", raw_image);
+		if (waitKey(30) >= 0) break;
+
     //std::list<std::pair<double, double> > c_list = CornerDetector::FindCorners(gray_image, gray_image.cols, gray_image.rows, 0.5, 4);
     //cout << c_list.size() << endl;
-
-    medianBlur(gray_image, blur_image, 5);
-
+    //medianBlur(gray_image, blur_image, 3);
     //blur_image = Util::GradientImage(gray_image, gray_image.cols, gray_image.rows) > 30;
-   
-    imshow("Test", CornerDetector::CornerImage(blur_image, 0.7, 4, gray_image.cols, gray_image.rows));
-    
+    //imshow("Test", CornerDetector::CornerImage(gray_image, 0.7, 3, gray_image.cols, gray_image.rows));
     //imshow("Test", blur_image);
-    waitKey(50);
   }
 
 }
